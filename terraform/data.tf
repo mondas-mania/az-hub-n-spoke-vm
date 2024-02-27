62e79dd1589708b58dd0fb99f04752a72ab77cf8
David Sampson <david.sampson@mobilise.cloud>
2024-02-27 10:51:12 +0000

    Add peerings between VNets

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,8 +2,13 @@ data "azurerm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
 
+data "azurerm_virtual_network" "ingress_vpc" {
+  name                = var.ingress_vnet_name
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+}
+
 data "azurerm_subnet" "ingress_subnet" {
   name                 = var.ingress_subnet_name
-  virtual_network_name = var.ingress_vnet_name
-  resource_group_name  = var.resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.ingress_vpc.name
+  resource_group_name  = data.azurerm_resource_group.resource_group.name
 }
