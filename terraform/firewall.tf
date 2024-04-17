24f56f8e5fa6be5463e68ea306aa8f4a2fce5fb9
David Sampson <david.sampson@mobilise.cloud>
2024-04-17 14:39:32 +0100

    Adding generla to do list

---
+++ terraform/firewall.tf
@@ -0,0 +1,6 @@
+# to do
+# 1. create PIP (standard, static)
+# 2. create basic firewall policy (firewall rules are outdated)
+# 3. create firewall resource
+# 4. make sure central nat gateway will only attach to firewall subnet if enabled
+# 5. make sure routing goes towards firewall - how will this work?
